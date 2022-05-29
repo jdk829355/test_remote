0034d502607703050ab632db0edc59b754d91f05
Daegyun Jung <jdk829355@gmail.com>
2022-05-29 22:48:48 +0900

    expression_pracrice

--- app/src/main/java/com/example/androidlab/Test.kt
+++ app/src/main/java/com/example/androidlab/Test.kt
@@ -2,11 +2,12 @@ package com.example.androidlab
 
 fun main(){
     var data = 10
-    if (data > 10){
-        println("data > 10")
-    }else if (data > 0 && data <= 10){
-        println("0 < data <= 10")
+    var result = if (data>0){
+        println("data > 0")
+        true
     }else{
         println("data <= 0")
+        false
     }
+    println(result)
 }
