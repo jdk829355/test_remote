b4aab1a19995e2509f2e2d57b2246b7b89ae0f93
Daegyun Jung <jdk829355@gmail.com>
2022-05-29 22:29:42 +0900

    collection_type_practice

--- app/src/main/java/com/example/androidlab/Test.kt
+++ app/src/main/java/com/example/androidlab/Test.kt
@@ -1,2 +1,19 @@
 package com.example.androidlab
 
+fun main(){
+    var data1 = listOf<Int>(10, 20, 30)
+    var data2 = mutableListOf<Int>(10, 20, 30)
+    var data3 = mapOf<String, String>("One" to "Uno", "Two" to "Tres")
+    println("""List size: ${data1.size}
+        List data: ${data1[0]}, ${data1[1]}, ${data1[2]}
+    """.trimIndent())
+    data2[2] = 40
+    println("""List size: ${data2.size}
+        List data: ${data2[0]}, ${data2[1]}, ${data2[2]}
+    """.trimIndent())
+    fun EngToSpa(st: String): String? {
+        return data3.get(st)
+    }
+    println(EngToSpa("One"))
+    println(EngToSpa("Two"))
+}
